acd93671abf181d8327fbefa3929c33453795a33
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-02-02 12:49:41 +0200

    add Loader

--- src/components/ImagePage/ImagePage.jsx
+++ src/components/ImagePage/ImagePage.jsx
@@ -1,22 +1,32 @@
 import { Component } from 'react';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { getImages } from 'services/imagesApi';
+import { Loader } from 'components/Loader/Loader';
 
 export class ImagePage extends Component {
   state = {
     images: [],
+    error: null,
+    isLoading: false,
   };
   async componentDidUpdate(prevProps, prevState) {
     //props -> query
     if (prevProps.query !== this.props.query) {
-
-        // console.log(this.props.query);
-
+      // console.log(this.props.query);
+      this.setState({
+        isLoading: true,
+      });
       try {
         const data = await getImages(this.props.query);
 
         this.setState({ images: data.hits });
-      } catch (error) {}
+      } catch (error) {
+        this.setState({ error: error.message });
+      } finally {
+        this.setState({
+          isLoading: false,
+        });
+      }
     }
   }
 
@@ -24,6 +34,8 @@ export class ImagePage extends Component {
     return (
       <>
         <ImageGallery images={this.state.images} />
+        {this.state.isLoading && <Loader />}
+
         {/* <Button/> */}
       </>
     );
